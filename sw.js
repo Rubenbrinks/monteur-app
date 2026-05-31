27d9ca2926afaccf1cc10debfe3ef15b4880308f
Rubenbrinks <rubenbrinks1@gmail.com>
2026-05-31 16:52:45 +0200

    1

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ── Emondt Materiaalapp – Service Worker ──────────────────────
-const CACHE_NAAM = 'emondt-materiaalapp-v2.0.1';
+const CACHE_NAAM = 'emondt-materiaalapp-v3.1.1';
 
 const TE_CACHEN = [
   './index.html',
